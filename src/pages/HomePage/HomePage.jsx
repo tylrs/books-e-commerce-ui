6debdc8589f655871d80be0e1a3b651db43bcc74
Taylor Galloway <galloway.taylor@gmail.com>
2024-02-09 10:31:14 -0600

    Add basic Homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import LoginButton from "../../components/LoginButton/LoginButton"
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <LoginButton />
+    </div>
+  )
+}
+
+export default HomePage
