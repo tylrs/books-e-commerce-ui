13f03e31dcdc4a2c005bacec08b9f60a284914eb
Taylor Galloway <galloway.taylor@gmail.com>
2024-02-11 11:32:32 -0600

    Local storge can be cleared

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,23 +1,7 @@
-/* 
-  take a keyName as a param
-  return item, setItem
-
-  tests
-    1) return initial value on component mount
-    2) return and use setter
-    3) should be able to do this for more complex JSON
-    4) should initialize state from localstorage
-    5) should set localstorage and state at the same time
-    6) state updates should trigger localstorage updates
-  
-    later
-      should be able to take an initial value
-*/
-
 import { useEffect, useState } from "react"
 
-export const useLocalStorage = (key) => {
-  const localStorageItem = JSON.parse(localStorage.getItem(key))
+export const useLocalStorage = (key, initialValue) => {
+  const localStorageItem = JSON.parse(localStorage.getItem(key)) || initialValue
   const setLocalStorageItem = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value))
   }
@@ -25,7 +9,11 @@ export const useLocalStorage = (key) => {
   const [state, setState] = useState(localStorageItem)
 
   useEffect(() => {
-    setLocalStorageItem(key, state)
+    if (state === "CLEARLOCALSTORAGE") {
+      localStorage.removeItem(key)
+    } else if (state !== undefined) {
+      setLocalStorageItem(key, state)
+    }
   }, [state])
 
   return [state, setState]
