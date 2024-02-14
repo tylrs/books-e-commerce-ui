5b565f797acdc73ff0909fa6f43d8dffc50fa206
Taylor Galloway <galloway.taylor@gmail.com>
2024-02-13 20:34:48 -0600

    Improve log out button styling

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -5,7 +5,7 @@ import { useContext, useRef } from "react"
 
 const LogoutButton = ({ handleClick }) => {
   return (
-    <button onClick={handleClick}>
+    <button className="flex items-center gap-4 border border-black rounded-lg py-2 px-6 whitespace-nowrap hover:bg-neutral-100" onClick={handleClick}>
       Log Out
     </button>
   )
@@ -23,8 +23,8 @@ const AuthModal = ({ setShowAuthModal }) => {
   useOutsideClickAction(ref, closeModal)
 
   return (
-    <div ref={ref} className="absolute flex flex-col gap-4 items-center border border-black rounded-lg right-0 top-8 py-10 px-20 z-10 bg-white">
-      {isLoggedIn ? <p>{user.full_name}</p> : <p>Welcome!</p>}
+    <div ref={ref} className="absolute flex flex-col items-center gap-4 items-center border border-black rounded-lg right-0 top-8 py-10 px-20 z-10 bg-white">
+      {isLoggedIn ? <p className="whitespace-nowrap">{`Welcome ${user.full_name}!`}</p> : <p>Welcome!</p>}
       {isLoggedIn ? <LogoutButton handleClick={handleLogOut} /> : <LoginButton />}
     </div>
   )
